10bb159142944d053fc38bb033a2c0b6c46dc584
lohitpt252003 <lohitpt252003@gmail.com>
2025-01-21 20:18:49 +0530

    OVERALL INIT

--- backend/app.py
+++ backend/app.py
@@ -1,11 +1,20 @@
-from flask import Flask
+from flask import Flask, jsonify
+from flask_cors import CORS
 
 app = Flask(__name__)
+CORS(app, origin='http://localhost:3000/')  # Enable CORS for all routes
 
 @app.route('/')
 def hello_world():
-    obj = {'numbers': [1, 2, 3, 4]}
-    return obj
+    return jsonify(
+        {
+            "names": [
+                "name1",
+                "name2",
+                "name3"
+            ]
+        }
+    )
 
 if __name__ == '__main__':
     app.run(debug=True)
--- frontend/src/Components/LandingPage/LandingPage.jsx
+++ frontend/src/Components/LandingPage/LandingPage.jsx
@@ -1,8 +1,25 @@
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
 
 function LandingPage() {
+    const [names, setNames] = useState([]);
+
+    useEffect(() => {
+        let apiCall = async () => {
+            const response = await axios.get('http://127.0.0.1:5000/');
+            console.log(response.data.names);
+            setNames(response.data.names);
+        }
+        apiCall();
+    }, []); // Empty dependency array to call the API only once when the component mounts
+
     return (
         <div>
-            <h1>Hi</h1>
+            {
+                names.map((name, index) => {
+                    return <h1 key={index}>{name}</h1>; // Added return and key
+                })
+            }
         </div>
     );
 }
